4bfca119c3e660d20c4a4332c288d79849539a42
Haji <tu201806253@aztu.local>
2022-01-26 13:55:25 +0400

    first commit

---
+++ enno2/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using enno2.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace enno2.Data
+{
+    public class AppDbContext: IdentityDbContext
+    {
+        
+        public AppDbContext(DbContextOptions options):base(options)
+        {
+        }
+
+        public DbSet<Team> teams { get; set; }
+
+    }
+}
